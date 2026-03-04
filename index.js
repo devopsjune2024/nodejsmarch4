beb57560be66276d4b1ed6f3dbb75263b196cc03
devopsjune2024 <devops.trainer12@gmail.com>
2026-03-04 07:04:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Filename: index.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('nodejs running in kubernetes!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
