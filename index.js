e8325d0b346d6adb47e90265e612aa157903b7c6
devopsjune2024 <devops.trainer12@gmail.com>
2026-03-04 21:33:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('nodejs running in kubernetes!');
+  res.send('NODEJSS ON kubernetes!');
 });
 
 app.listen(port, () => {
